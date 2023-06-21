3e2cacfee8ea5e1910e7e01b6be72f411f95c9e8
dario.casciato <dario.casciato@dormakaba.com>
2023-06-21 12:42:30 +0200

    declared function for conversion, created command

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,76 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    console.log('Congratulations, your extension "html-to-scss" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "html-to-scss" is now active!');
+    // Register the command to generate SCSS from selected HTML
+    let disposable = vscode.commands.registerCommand('extension.generateSCSS', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const selection = editor.selection;
+            const htmlSnippet = editor.document.getText(selection);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('html-to-scss.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from HTML to SCSS!');
-	});
+            const scssSnippet = generateSCSS(htmlSnippet);
 
-	context.subscriptions.push(disposable);
+            vscode.env.clipboard.writeText(scssSnippet);
+            vscode.window.showInformationMessage('SCSS snippet generated and copied to clipboard!');
+        }
+    });
+
+    // Add the command as a context menu item
+    let contextDisposable = vscode.commands.registerTextEditorCommand('extension.generateSCSSContextMenu', (textEditor: vscode.TextEditor) => {
+        const selection = textEditor.selection;
+        const htmlSnippet = textEditor.document.getText(selection);
+
+        const scssSnippet = generateSCSS(htmlSnippet);
+
+        vscode.env.clipboard.writeText(scssSnippet);
+        vscode.window.showInformationMessage('SCSS snippet generated and copied to clipboard!');
+    });
+
+    context.subscriptions.push(disposable, contextDisposable);
 }
 
-// This method is called when your extension is deactivated
+function generateSCSS(htmlSnippet: string): string {
+	let indentation = '';
+	let scssCode = '';
+
+	// Split the HTML snippet into separate lines
+	const lines = htmlSnippet.split('\n');
+
+	// Iterate over each line of the HTML snippet
+	for (let line of lines) {
+	  // Remove leading and trailing whitespace
+	  line = line.trim();
+
+	  // Ignore empty lines
+	  if (line.length === 0) {
+		continue;
+	  }
+
+	  // Determine the indentation level
+	  const match = line.match(/^\s+/);
+	  if (match && match[0]) {
+		indentation = match[0];
+	  }
+
+	  // Remove the existing indentation from the line
+	  line = line.replace(/^\s+/, '');
+
+	  // Open the SCSS block
+	  scssCode += indentation + line + ' {\n';
+
+	  // Increase the indentation level
+	  indentation += '  ';
+	}
+
+	// Close the SCSS blocks
+	while (indentation.length >= 2) {
+	  indentation = indentation.slice(0, -2);
+	  scssCode += indentation + '}\n';
+	}
+
+	return scssCode;
+}
+
 export function deactivate() {}
