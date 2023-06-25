67fea8534e25ef881ca6fb6a0917a79ffa8a2720
Dario Casciato <93987804+DarioCasciato@users.noreply.github.com>
2023-06-25 14:56:48 +0200

    generateSCSS in separate file

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import { generateSCSS } from './generate-scss';
 
 export function activate(context: vscode.ExtensionContext) {
     console.log('Congratulations, your extension "html-to-scss" is now active!');
@@ -31,46 +32,5 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(disposable, contextDisposable);
 }
 
-function generateSCSS(htmlSnippet: string): string {
-	let indentation = '';
-	let scssCode = '';
-
-	// Split the HTML snippet into separate lines
-	const lines = htmlSnippet.split('\n');
-
-	// Iterate over each line of the HTML snippet
-	for (let line of lines) {
-	  // Remove leading and trailing whitespace
-	  line = line.trim();
-
-	  // Ignore empty lines
-	  if (line.length === 0) {
-		continue;
-	  }
-
-	  // Determine the indentation level
-	  const match = line.match(/^\s+/);
-	  if (match && match[0]) {
-		indentation = match[0];
-	  }
-
-	  // Remove the existing indentation from the line
-	  line = line.replace(/^\s+/, '');
-
-	  // Open the SCSS block
-	  scssCode += indentation + line + ' {\n';
-
-	  // Increase the indentation level
-	  indentation += '  ';
-	}
-
-	// Close the SCSS blocks
-	while (indentation.length >= 2) {
-	  indentation = indentation.slice(0, -2);
-	  scssCode += indentation + '}\n';
-	}
-
-	return scssCode;
-}
 
 export function deactivate() {}
---
+++ src/generate-scss.ts
@@ -0,0 +1,41 @@
+export function generateSCSS(htmlSnippet: string): string {
+	let indentation = '';
+	let scssCode = '';
+
+	// Split the HTML snippet into separate lines
+	const lines = htmlSnippet.split('\n');
+
+	// Iterate over each line of the HTML snippet
+	for (let line of lines) {
+	  // Remove leading and trailing whitespace
+	  line = line.trim();
+
+	  // Ignore empty lines
+	  if (line.length === 0) {
+		continue;
+	  }
+
+	  // Determine the indentation level
+	  const match = line.match(/^\s+/);
+	  if (match && match[0]) {
+		indentation = match[0];
+	  }
+
+	  // Remove the existing indentation from the line
+	  line = line.replace(/^\s+/, '');
+
+	  // Open the SCSS block
+	  scssCode += indentation + line + ' {\n';
+
+	  // Increase the indentation level
+	  indentation += '  ';
+	}
+
+	// Close the SCSS blocks
+	while (indentation.length >= 2) {
+	  indentation = indentation.slice(0, -2);
+	  scssCode += indentation + '}\n';
+	}
+
+	return scssCode;
+}
