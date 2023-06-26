f7263634d4517e97f05c715a8ce040e0d36ebcb3
Dario Casciato <93987804+DarioCasciato@users.noreply.github.com>
2023-06-26 21:17:35 +0200

    working function, but not with large snippets

--- src/generate-scss.ts
+++ src/generate-scss.ts
@@ -1,44 +1,78 @@
 export function generateSCSS(htmlSnippet: string): string {
-	let indentation = '';
+	const openingTagRegex = /<(\w+)(?:\s[^>]*?\bclass\s*=\s*["'](\w+))?[^>]*>|<\/(\w+)>/g;
+	const selfClosingTagRegex = /<(\w+)(?:\s[^>]*?\bclass\s*=\s*["'](\w+))?[^>]*\s*\/>/g;
+	const closingTagRegex = /<\/(\w+)>/g;
 	let scssCode = '';
+	let indentLevel = 0;
+	let isSelfClosing = false;
 
-	// Split the HTML snippet into separate lines
-	const lines = htmlSnippet.split('\n');
+	const uniqueTags = new Set<string>();
+	const openingTags = [];
 
-	// Iterate over each line of the HTML snippet
-	for (let line of lines) {
-	  // Remove leading and trailing whitespace
-	  line = line.trim();
+	let match;
+	while ((match = openingTagRegex.exec(htmlSnippet)) !== null) {
+	  const [, openingTag, className, closingTag] = match;
 
-	  // Ignore empty lines
-	  if (line.length === 0) {
-		continue;
+	  if (openingTag) {
+		const tagKey = `${openingTag}.${className}`;
+		if (uniqueTags.has(tagKey)) {
+		  continue; // Skip duplicate opening tag
+		}
+		uniqueTags.add(tagKey);
+
+		const indent = '\t'.repeat(indentLevel);
+		let selector = openingTag;
+		if (openingTag === 'div' && className) {
+		  selector = `.${className}`;
+		} else if (className) {
+		  selector += `.${className}`;
+		}
+		const openingBlock = `${indent}${selector} {\n\n`;
+		scssCode += openingBlock;
+		openingTags.push(openingTag);
+		indentLevel++;
+	  } else if (closingTag) {
+		if (isSelfClosing) {
+		  isSelfClosing = false;
+		  continue; // Skip closing tag for self-closing tag
+		}
+		indentLevel--;
+		const indent = '\t'.repeat(indentLevel);
+		const closingSelector = closingTag;
+		const closingBlock = `${indent}}\n`;
+		scssCode += closingBlock;
+		openingTags.pop();
 	  }
-
-	  // Determine the indentation level
-	  const match = line.match(/^\s+/);
-	  if (match && match[0]) {
-		indentation = match[0];
-	  }
-
-	  // Remove the existing indentation from the line
-	  line = line.replace(/^\s+/, '');
-
-	  // Open the SCSS block
-	  scssCode += indentation + line + ' {\n';
-
-	  // Increase the indentation level
-	  indentation += '  ';
 	}
 
-	// Close the SCSS blocks
-	while (indentation.length >= 2) {
-	  indentation = indentation.slice(0, -2);
-	  scssCode += indentation + '}\n';
+	while ((match = selfClosingTagRegex.exec(htmlSnippet)) !== null) {
+	  const [, selfClosingTag, selfClosingClassName] = match;
+	  isSelfClosing = true;
+
+	  const tagKey = `${selfClosingTag}.${selfClosingClassName}`;
+	  if (uniqueTags.has(tagKey)) {
+		continue; // Skip duplicate self-closing tag
+	  }
+	  uniqueTags.add(tagKey);
+
+	  const indent = '\t'.repeat(indentLevel);
+	  let selector = selfClosingTag;
+	  if (selfClosingTag === 'div' && selfClosingClassName) {
+		selector = `.${selfClosingClassName}`;
+	  } else if (selfClosingClassName) {
+		selector += `.${selfClosingClassName}`;
+	  }
+	  const selfClosingBlock = `${indent}${selector} {\n\n}\n`;
+	  scssCode += selfClosingBlock;
+	}
+
+	while (openingTags.length > 0) {
+	  indentLevel--;
+	  const indent = '\t'.repeat(indentLevel);
+	  const openingTag = openingTags.pop();
+	  const closingBlock = `${indent}}\n`;
+	  scssCode += closingBlock;
 	}
 
 	return scssCode;
-}
-
-
-//Regex for normal object opener:  <(\w+)(?:\s[^>]*?\bclass\s*=\s*["\'](\w+))?[^>]*>
+  }
