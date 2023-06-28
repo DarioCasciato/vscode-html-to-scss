9bb93a70169abb30938b1e1924672525225ee565
dario.casciato <dario.casciato@dormakaba.com>
2023-06-28 08:25:31 +0200

    functional function

--- src/generate-scss.ts
+++ src/generate-scss.ts
@@ -1,78 +1,67 @@
+import * as htmlparser2 from 'htmlparser2';
+import { DomHandler, Element } from 'domhandler';
+
 export function generateSCSS(htmlSnippet: string): string {
-	const openingTagRegex = /<(\w+)(?:\s[^>]*?\bclass\s*=\s*["'](\w+))?[^>]*>|<\/(\w+)>/g;
-	const selfClosingTagRegex = /<(\w+)(?:\s[^>]*?\bclass\s*=\s*["'](\w+))?[^>]*\s*\/>/g;
-	const closingTagRegex = /<\/(\w+)>/g;
-	let scssCode = '';
-	let indentLevel = 0;
-	let isSelfClosing = false;
+  let scssCode = '';
+  let indentLevel = 0;
+  let isSelfClosing = false;
 
-	const uniqueTags = new Set<string>();
-	const openingTags = [];
+  const uniqueTags = new Set<string>();
+  const openingTags: string[] = [];
 
-	let match;
-	while ((match = openingTagRegex.exec(htmlSnippet)) !== null) {
-	  const [, openingTag, className, closingTag] = match;
+  const handler = new DomHandler((error, dom) => {
+    if (error) {
+      throw new Error(`Error parsing HTML: ${error}`);
+    }
 
-	  if (openingTag) {
-		const tagKey = `${openingTag}.${className}`;
-		if (uniqueTags.has(tagKey)) {
-		  continue; // Skip duplicate opening tag
-		}
-		uniqueTags.add(tagKey);
+    function processElement(element: Element) {
+      const { tagName, attribs, children } = element;
 
-		const indent = '\t'.repeat(indentLevel);
-		let selector = openingTag;
-		if (openingTag === 'div' && className) {
-		  selector = `.${className}`;
-		} else if (className) {
-		  selector += `.${className}`;
-		}
-		const openingBlock = `${indent}${selector} {\n\n`;
-		scssCode += openingBlock;
-		openingTags.push(openingTag);
-		indentLevel++;
-	  } else if (closingTag) {
-		if (isSelfClosing) {
-		  isSelfClosing = false;
-		  continue; // Skip closing tag for self-closing tag
-		}
-		indentLevel--;
-		const indent = '\t'.repeat(indentLevel);
-		const closingSelector = closingTag;
-		const closingBlock = `${indent}}\n`;
-		scssCode += closingBlock;
-		openingTags.pop();
-	  }
-	}
+      if (tagName) {
+        const classNames = attribs.class ? attribs.class.split(' ') : [];
+        const tagKey = `${tagName}.${classNames.join('.')}`;
+        if (uniqueTags.has(tagKey)) {
+          return; // Skip duplicate tag
+        }
+        uniqueTags.add(tagKey);
 
-	while ((match = selfClosingTagRegex.exec(htmlSnippet)) !== null) {
-	  const [, selfClosingTag, selfClosingClassName] = match;
-	  isSelfClosing = true;
+        const indent = '\t'.repeat(indentLevel);
+        let selector = '';
+        if (tagName !== 'div') {
+          selector = tagName;
+        }
+        if (classNames.length > 0) {
+          selector += `.${classNames.join('.')}`;
+        }
 
-	  const tagKey = `${selfClosingTag}.${selfClosingClassName}`;
-	  if (uniqueTags.has(tagKey)) {
-		continue; // Skip duplicate self-closing tag
-	  }
-	  uniqueTags.add(tagKey);
+        const openingBlock = `${indent}${selector} {\n\n`;
+        scssCode += openingBlock;
+        openingTags.push(tagName);
+        indentLevel++;
 
-	  const indent = '\t'.repeat(indentLevel);
-	  let selector = selfClosingTag;
-	  if (selfClosingTag === 'div' && selfClosingClassName) {
-		selector = `.${selfClosingClassName}`;
-	  } else if (selfClosingClassName) {
-		selector += `.${selfClosingClassName}`;
-	  }
-	  const selfClosingBlock = `${indent}${selector} {\n\n}\n`;
-	  scssCode += selfClosingBlock;
-	}
+        for (const child of children) {
+          if (child.type === 'tag') {
+            processElement(child as Element);
+          }
+        }
 
-	while (openingTags.length > 0) {
-	  indentLevel--;
-	  const indent = '\t'.repeat(indentLevel);
-	  const openingTag = openingTags.pop();
-	  const closingBlock = `${indent}}\n`;
-	  scssCode += closingBlock;
-	}
+        indentLevel--;
+        const closingBlock = `${indent}}\n`;
+        scssCode += closingBlock;
+        openingTags.pop();
+      }
+    }
 
-	return scssCode;
-  }
+    for (const element of dom) {
+      if (element.type === 'tag') {
+        processElement(element as Element);
+      }
+    }
+  });
+
+  const parser = new htmlparser2.Parser(handler);
+  parser.write(htmlSnippet);
+  parser.end();
+
+  return scssCode;
+}
